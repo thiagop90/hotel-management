4f69bac8a3a6b975456cc5f9d502172b3213e272
Thiago <thgps01@outlook.com>
2025-06-20 12:53:59 -0300

    style: ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
             <ScrollBar orientation="horizontal" />
           </ScrollArea>
 
-          <div className="my-8 space-y-4">
+          <div className="my-4 space-y-4">
             <HotelCalendar
               rooms={standardRooms}
               reservations={standardReservations}
